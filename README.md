041a48d80c0a0503ed2f693359dd61c88d011234
Daniel <danieltadashi2004@gmail.com>
2024-10-04 19:07:01 -0500

    Modificacion de ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Prueba
  Repositorio para practicar nuevos temas
+Ultimo cambio: Creacion de programa de prueba en python / java
---
+++ programa_prueba3.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Incluir la biblioteca estándar de entrada/salida
+
+int main() {
+    printf("Hola, mundo!\n"); // Imprimir en la consola
+    return 0; // Indica que el programa terminó correctamente
+}
